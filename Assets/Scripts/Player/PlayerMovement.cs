6fab18e5e79bbfd5748386843e44b61dda157715
D'alessandro Ethan <roger.paldi@yahoo.com>
2023-11-09 16:10:09 +0100

    Player Basic Movement and Shooting

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,41 +9,66 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Vector3 dir;
     [SerializeField] private Vector3 target;
 
+    [Header("PlayerStatus")]
+    [SerializeField] private bool shooting;
+
     [Header("PlayerProperties")]
-    [SerializeField, Range(0,100)] private float moveSpeed;
-    [SerializeField, Range(0,100)] private float rotateSpeed;
-    [SerializeField, Range(0,100)] private float jumpForce;
+    [SerializeField, Range(0, 100)] private float moveSpeed;
+    private float moveSpeedOrigin;
+    [SerializeField, Range(0, 100)] private float moveSpeedWhileShooting;
+    [SerializeField, Range(0, 100)] private float rotateSpeed;
+    [SerializeField, Range(0, 100)] private float jumpForce;
+    [SerializeField] private float fireRate;
+    private float shootDelay;
 
     [Header("Player Component")]
+    [SerializeField] private GameObject projectilePrefab;
     private Rigidbody body;
 
     private void Awake()
     {
+        moveSpeedOrigin = moveSpeed;
         body = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
         PlayerMove();
-        PlayerRotation();
-
-        RaycastHit rayHit;
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (Physics.Raycast(ray, out rayHit) && (rayHit.transform.CompareTag("Ground") || (rayHit.transform.CompareTag("Obstacle"))))
-        {
-            target = rayHit.point - transform.position;
-        }
-        Debug.DrawRay(transform.position, target);
+        PlayerRotationAndAim();
+        Shooting();
+        Debug.DrawRay(transform.position, target, Color.yellow);    
     }
 
-    private void PlayerRotation()
+    private void Shooting()
+    {
+        if (shooting)
+        { 
+            if(moveSpeed != moveSpeedWhileShooting)
+            {
+                StartCoroutine(moveSpeedSlowing());
+            }
+            shootDelay += Time.fixedDeltaTime;
+            if (shootDelay >= fireRate)
+            {
+                GameObject actualProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.LookRotation(target));
+                shootDelay = 0;
+            }
+        }
+        else
+        {
+            shootDelay += Time.fixedDeltaTime;
+            shootDelay = Mathf.Clamp(shootDelay, 0, fireRate);
+        }
+    }
+
+    private void PlayerRotationAndAim()
     {
         RaycastHit rayHit;
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
 
         if (Physics.Raycast(ray, out rayHit) && (rayHit.transform.CompareTag("Ground") || (rayHit.transform.CompareTag("Obstacle"))))
         {
+            target = rayHit.point - transform.position;
             dir = rayHit.point - transform.position;
             dir.y = 0;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.fixedDeltaTime * rotateSpeed);
@@ -56,7 +82,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Movement(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if (context.performed)
         {
             moveDirection = context.ReadValue<Vector2>();
         }
@@ -64,5 +90,24 @@ public class PlayerMovement : MonoBehaviour
         {
             moveDirection = Vector2.zero;
         }
+    }
+
+    public void Shooting(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            shooting = true;
+        }
+        else
+        {
+            shooting = false;
+        }
+    }
+
+    public IEnumerator moveSpeedSlowing()
+    {
+        moveSpeed = moveSpeedWhileShooting;
+        yield return new WaitForSeconds(0.5f);
+        moveSpeed = moveSpeedOrigin;
     }
 }
